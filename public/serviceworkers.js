ce0e1d12565cfbb7edb80d8ddcad0e81ab428df7
Recep Taylanhan <rcptay@gmail.com>
2023-07-17 00:18:41 -0400

    practice with promises and Fetch API

--- public/serviceworkers.js
+++ public/serviceworkers.js
@@ -1,5 +1,5 @@
 // service workers are running in the background and all about handling events.
-// Therefore we allways attach event listeners to the service worker,
+// Therefore we always attach event listeners to the service worker,
 //we simply reatc to events.we do this by referrring with self keyword and then we add event listener after that.
 self.addEventListener("install", function(event){ // we do not have any DOM events in the SWs but we have special set of events such as install, etc...
      console.log("[service worker] Installing Service Worker ...", event)
--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,15 +1,68 @@
 var deferredPrompt;
-if("serviceWorker" in navigator){
-    navigator.serviceWorker
-    .register("/serviceworkers.js")// / slash means to make sure we allways go to the root folder(public) first and then go to the our sws file name // register actually return a promise.
-    .then(function(){
-        console.log("Service worker registered");
+if("serviceWorker" in navigator){ 
+    navigator.serviceWorker  // first we acces to the service worker 
+    .register("/serviceworkers.js")// then we call register .register is just a method for service worker/ slash means to make sure we always go to the root folder(public) first and then go to the our sws file name // register actually return a promise.
+    .then(function(){ // so service worker API return us a promise if servise worker registered.
+        // console.log("Service worker registered");
+    })
+    .catch(function(err){
+     console.log(err);
     })
 }    
 
 window.addEventListener("beforeinstallprompt",function(event){
-    console.log("beforeinstallprompt fired");
+    // console.log("beforeinstallprompt fired");
     event.preventDefault();
     deferredPrompt = event;
     return false;
 })
+
+//example for handle async task in javascript
+var promise = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        // resolve("this is executed once the timer is done (after 3 second )!");
+        reject({code: 500, message: "An error occurred"});
+     },3000);
+});
+//Send GET HTTP Request
+fetch("https://httpbin.org/ip") // We are sending GET request here .but we can send any HTTP request beside GET if needed.
+   .then(function(response){
+    console.log(response)
+    return response.json(); //we need to parse response data always before use it.
+   })
+   .then(function(data){
+    console.log(data)
+   })
+   .catch(function(err){
+    console.log(err)
+   });
+
+   //Send POST HTTP Request
+   fetch("https://httpbin.org/post",{
+    method:"POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify({ message: "Does this work?"})
+   }) // We are sending GET request here .but we can send any HTTP request beside GET if needed.
+   .then(function(response){
+    console.log(response)
+    return response.json(); //we need to parse response data always before use it.
+   })
+   .then(function(data){
+    console.log(data)
+   })
+   .catch(function(err){
+    console.log(err)
+   });
+
+
+promise.then(function(text){
+    return text;
+}).then(function(newText){
+    console.log(newText);
+}).catch(function(err){
+    console.log(err.code, err.message);
+});
+console.log("this is executed right after setTimeout")
